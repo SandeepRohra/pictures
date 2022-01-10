58e93d7115e280ab71fe54f8d882e8fa5364697d
Sandeep Rohra <sandeep12rohra@gmail.com>
2022-01-10 21:29:48 +0530

    added notes forhow the search is hapenning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,12 @@ class App extends React.Component {
   state = { images: [] } //yaha p apan state ko array m isiliye set kiye taki baad m apan map kar paye images ko
 
   onSearchSubmit = async (term) => {
+    //$ INP6 ab  upar (isme) jo term h vo searchbar k state k term k equal hua n to fir vo pass ho jayega isme
+
+    //INP7 to vo isme term input vala query key k sath h to vohich search hoga jo input m type hoga
+
+    ///-------------Finished IMP points which explained how the Search is Happening-----------///
+
     //isliye kaam nii kiya tha arrow fuction  kyuki async = k adar ayega
     const response = await unsplash //apan y refector kiye iske contents ko api k unsplash.js m daaldiye
       .get('/search/photos', {
@@ -15,7 +21,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container" style={{ marginTop: '15px' }}>
-        <Input onSubmit={this.onSearchSubmit} />
+        <Input onInputSubmit={this.onSearchSubmit} />
+        {/* IMP1  yaha p onInputSubmit h vo ek prop banke SearchBar m gyii  ab searchBAr vale Component m jaa IMP2 m */}
+
+        {/* @ IMP5 ab onInputSubmit ki value term k equal h jo ki onChange k karan change hoke jo type kiye h vo ho gyi h and vo ab isme paas hogi 
+         ab sidha $ INP6 vale comment m jaa jo upar h */}
         <p>Found {this.state.images.length} images</p>
       </div>
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,8 +8,11 @@ class Input extends React.Component {
   //verna vo form submitkardeta h normally
   onSubmit = (event) => {
     event.preventDefault()
-    //Yaha p kyuki vo input ka prop h to usko apan yaha exesses kiye or usko apan this.state.term k sath imliment kar diye
-    this.props.onSubmit(this.state.term)
+    //#IMP2 Yaha p kyuki vo input ka prop h to to usko apan is function m use kiye
+
+    //#IMP3 yaha pas us onInputSubmit ki value ko apan this.state.term k equal kar diye abhi this.state.term kuch nii h
+    // ab sidha niche onChange() m jaa #IMP4
+    this.props.onInputSubmit(this.state.term)
   }
   render() {
     return (
@@ -20,11 +23,9 @@ class Input extends React.Component {
             <label>Image Search</label>
             <input
               type="text"
-              //   yaha p apan controled behaviour of input k liye uski value ko state term kiye
+              //#IMP4 ab vo state ki value yaha s change ho jaygi or term ki value b isiliye  change hojaygi n vo ho jaygi jo input m type hoga
 
-              //   and then apan onChange m function deke state ki value change kiye setState karke taki y controled element rahe
-
-              //   more precisely vo element k input ki value react p store rahe
+              //yaha s sidha app.js m @ VALA comment padhna IMP5
               onChange={(e) => {
                 this.setState({ term: e.target.value })
               }}
