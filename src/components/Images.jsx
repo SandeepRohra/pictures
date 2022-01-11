b1343662ceede1387e373dd8d90b216d5a91f03f
Sandeep Rohra <sandeep12rohra@gmail.com>
2022-01-11 13:52:30 +0530

    added another component whic gives image and from lacture 111

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './images-list.css'
+import ImageCard from './image-cars'
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.regular} /> //yaha p apan vo use state s image liye usko ek prop m dale andusko map kardiye
+    return <ImageCard key={image.id} image={image} /> //yaha p apan vo use state s image liye usko ek prop m dale andusko map kardiye
   })
   return <div className="image-list">{images}</div>
 }
